b0e21c58da4508cacdae4cec37cb8e0ce696ee51
Lucas <128625477+LucasXLR8@users.noreply.github.com>
2023-06-07 16:59:33 -0300

    Add files via upload

---
+++ exercicio 1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void maiorNumero(int n1,int n2){
+    int maior;
+    if(n1 > n2){
+        maior = n1;
+    }else{
+        maior = n2;
+    }
+    printf("Maior: %d\n", maior);
+}
+void menorNumero(int n1,int n2){
+    int menor;
+    if(n1 < n2){
+        menor = n1;
+    }else{
+        menor = n2;
+    }
+    printf("Menor: %d\n", menor);
+}
+int main(){
+
+    int primeiroNumero, segundoNumero;
+
+    printf("Digite o primeiro numero: \n");
+    scanf("%d", &primeiroNumero);
+
+    printf("Digite o segundo numero: \n");
+    scanf("%d", &segundoNumero);
+
+    maiorNumero(primeiroNumero,segundoNumero);
+    menorNumero(primeiroNumero,segundoNumero);
+
+    return 0;
+}
