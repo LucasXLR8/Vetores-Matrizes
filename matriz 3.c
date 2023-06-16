537d50726b2673736f67d2fd8a6242055a4e2c51
Lucas <128625477+LucasXLR8@users.noreply.github.com>
2023-06-16 18:23:33 -0300

    Update matriz 3.c

--- matriz 3.c
+++ matriz 3.c
@@ -2,14 +2,35 @@
 #include <stdlib.h>
 
 int main(){
-    
     char disciplinas[3][100];
     int notas[3][2];
-    int i,c,media[3];
+    int i,c,somaNotas,media[3];
 
-    for (i = 0; i < ; i++)
+    for (i = 0; i < 3; i++)
     {
-        /* code */
+        printf("Digite o nome da disciplina: \n",i + 1);
+        scanf("%s", &disciplinas[i]);
+        for (c = 0; c < 2 ; c++)
+        {
+            printf("Digite a %d nota: \n",c + 1);
+            scanf("%d", &notas[i][c]);
+            somaNotas += notas[i][c];
+        }
+        media[i] = somaNotas/c;
+        somaNotas = 0;   
+    }
+    system("cls");
+    for (i = 0; i < 3; i++)
+    {
+         printf("Disciplina: %s\n",disciplinas[i]);
+         for (c = 0; c < 2; c++)
+         {
+            printf("Nota: %d\n",notas[i][c]);
+         }
+         printf("Media: %d\n",media[i]);
+         printf("\n");
+         
     }
     
+    return 0;
 }
